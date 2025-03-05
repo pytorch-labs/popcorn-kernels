aefd127a8974536d3bb414242f1d81483172f8be
Simon Guo <simonguo@stanford.edu>
2025-03-04 22:31:05 -0800

    better categorization of operators

--- operators.py
+++ operators.py
@@ -2,9 +2,41 @@
 List of PyTorch Operators
 
 We will use this to combine the operators into synthetic programs.
+
+
+We can think about better naming and categorization of these operators.
 """
 
-main_operators = ["Conv2d", "Matmul", "Gemm", "BMM", "Conv3d", "ConvTranspose2d", "ConvTranspose3d", "Conv1d", "ConvTranspose1d", "Linear", "Bilinear", "LazyLinear", "LazyConv1d", "LazyConv2d", "LazyConv3d", "LazyConvTranspose1d", "LazyConvTranspose2d", "LazyConvTranspose3d"]
+# Core operators (Names could be better)
+# These are basic building blocks
+matrix = ["Matmul", "Gemm", "BMM"]
+linear_operators = ["Linear", "Bilinear", "LazyLinear"]
+
+convolutions = [
+    "Conv1d", "Conv2d", "Conv3d", 
+    "ConvTranspose1d", "ConvTranspose2d", "ConvTranspose3d",
+]
+
+# maybe not needed, this is for when you don't have dimension in the input specified
+lazy_convolutions = [
+    "LazyConv1d", "LazyConv2d", "LazyConv3d", 
+    "LazyConvTranspose1d", "LazyConvTranspose2d", "LazyConvTranspose3d"
+]
+
+core_operators = matrix + linear_operators + convolutions 
+
+# Compound operators (Names could be better)
+# These are more complex operators that already has a lot of operators inside
+# Are some of these too big?
+embedding = ["Embedding", "EmbeddingBag"]
+attention = ["MultiheadAttention"]
+recurrent = ["RNN", "LSTM", "GRU", "RNNCell", "LSTMCell", "GRUCell"]
+transformer = ["TransformerEncoderLayer", "TransformerDecoderLayer"]
+
+compound_operators = embedding + attention + recurrent + transformer
+
+#  Supporting operators (Names could be better)
+# These are operators that act on core operators
 
 activations = ["ReLU", "Sigmoid", "Tanh", "LeakyReLU", "GELU", "Swish", "Softmax", "Mish", "Hardtanh", "HardSwish", "ELU", "CELU", "SELU", "ReLU6", "PReLU", "RReLU", "LogSigmoid", "Softmin", "Softplus", "Softsign", "Softshrink", "Hardshrink", "Hardsigmoid", "Hardswish", "GLU", "SiLU", "Tanhshrink", "Threshold", "LogSoftmax", "Softmax2d"]
 element_wise_ops = ["Add", "Multiply", "Subtract", "Divide", "Clamp", "Scale", "ResidualAdd", "Identity", "CosineSimilarity", "PairwiseDistance"]
@@ -18,18 +50,11 @@ channel_ops = ["ChannelShuffle", "PixelShuffle", "PixelUnshuffle"]
 padding = ["ZeroPad1d", "ZeroPad2d", "ZeroPad3d", "ReflectionPad1d", "ReflectionPad2d", "ReflectionPad3d", "ReplicationPad1d", "ReplicationPad2d", "ReplicationPad3d", "CircularPad1d", "CircularPad2d", "CircularPad3d", "ConstantPad1d", "ConstantPad2d", "ConstantPad3d"]
 upsampling = ["Upsample", "UpsamplingNearest2d", "UpsamplingBilinear2d"]
 
-
-# these are starting to be big, questionable???
-embedding = ["Embedding", "EmbeddingBag"]
-attention = ["MultiheadAttention"]
-recurrent = ["RNN", "LSTM", "GRU", "RNNCell", "LSTMCell", "GRUCell", "RNNBase", "RNNCellBase"]
-transformer = ["Transformer", "TransformerEncoder", "TransformerDecoder", "TransformerEncoderLayer", "TransformerDecoderLayer"]
-
-# Update the all_operators list to include the new categories instead of "others"
-all_operators = activations + element_wise_ops + normalizations + pooling + bias + reductions + regularization + reshaping + channel_ops + padding + embedding + recurrent + transformer + upsampling + attention
-
-
+supporting_operators = activations + element_wise_ops + normalizations + pooling + bias + reductions + regularization + reshaping + channel_ops + padding + embedding + recurrent + transformer + upsampling + attention
 
 
 # Probably not good to have
-containers = ["Sequential", "ModuleList", "ModuleDict", "ParameterList", "ParameterDict", "Container", "Module"]
+# recurrent = ["RNNBase", "RNNCellBase"]
+
+# transformer = ["Transformer", "TransformerEncoder", "TransformerDecoder", "TransformerEncoderLayer", "TransformerDecoderLayer"]
+# containers = ["Sequential", "ModuleList", "ModuleDict", "ParameterList", "ParameterDict", "Container", "Module"]
